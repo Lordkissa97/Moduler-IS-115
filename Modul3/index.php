287c22b882b7f4ab37b2622566e525ccc5de595d
Lordkissa97 <christiaea@uia.no>
2024-09-22 19:24:34 +0200

    modul 3 start

---
+++ Modul3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Modul 3</h1>
+    <ul>
+        <li><a href="oppgv3_1.php">Datosjekk</a></li>
+        <li><a href="oppgv3_2.php">Teller med pauseknapp</a></li>
+        <li><a href="oppgv3_3.php">ny saldo</a></li>
+        <li><a href="oppgv3_4.php">Sjekk av fylkestilhørighet</a></li>
+        <li><a href="oppgv3_5.php">Sjakk og hvete</a></li>
+    </ul>
+</body>
+</html>
