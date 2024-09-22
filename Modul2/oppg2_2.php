be56c0b272ee986f0c9a4075c1b96299bb984eeb
Lordkissa97 <christiaea@uia.no>
2024-09-22 19:16:28 +0200

    modul 2

--- Modul2/oppg2_2.php
+++ Modul2/oppg2_2.php
@@ -3,11 +3,11 @@
 $etternavn = "<script>alert('hehehe');</script>Eieland";
 
 //htmlspecialchars konverterer spesialtegn til HTML-entiteter (erstatningstekt for tegn i HTML)
-//$sikkertEtternavn = htmlspecialchars($etternavn, ENT_QUOTES, 'UTF-8');
+$sikkertEtternavn = htmlspecialchars($etternavn, ENT_QUOTES, 'UTF-8');
 
 //bruker strip_tags for å fjerne HTML- og PHP-koder i $etternavn
-$sikkertEtternavn = strip_tags($etternavn);
+//$sikkertEtternavn = strip_tags($etternavn);
 
-echo "Behandlet etternavn: " . $etternavn . "<br>";
+echo "Behandlet etternavn: " . $sikkertEtternavn . "<br>";
 
 ?>
--- Modul2/oppg2_4.php
+++ Modul2/oppg2_4.php
@@ -1,13 +1,13 @@
 <?php
 
 $tall1 = 546;
-$tall2 = 232;
+$tall2 = 1000;
 
 //lage kalkulator med funksjon
 
 function Differanse($tall1, $tall2) {
     //kalkulatoren
-    $difference = $tall1 - $tall2;
+    $difference = abs($tall1 - $tall2);
     //utskrift
     echo "Differansen mellom $tall1 og $tall2 er $difference.";
 }
