8494d476142eec63562fd8ab860c4c9b03f7a69f
Lordkissa97 <christiaea@uia.no>
2024-09-23 21:03:55 +0200

    oppgave5

---
+++ Modul3/oppgv3_5.php
@@ -0,0 +1,33 @@
+<?php
+//Sjakkbrett totalt ruter 8x8 = 64
+$Sjakkbrett = 64;
+
+//vekt på hvetekorn i gram
+$vektPrKorn = 0.035;
+
+//antall gram i 1tonn (1million)
+$gramPrTonn = 1000000;
+
+//Lage Array som lagrer antall korn for hver rute
+$kornPrRute = [];
+
+//legger til korn for første rute
+$kornPrRute[0] = 1;
+
+//bruker for-løkke til å beregne antall korn pr rute med å doble antallet for hver
+for ( $i = 1; $i < $Sjakkbrett; $i++ ) {
+    $kornPrRute[$i] = $kornPrRute[$i - 1] * 2;
+}
+
+//deretter beregne totalt antall korn ved å summere array
+$kornTotalt = array_sum($kornPrRute);
+
+//kovertere antall korn i gram over til tonn
+$kornTonn = $kornTotalt * $vektPrKorn / $gramPrTonn;
+
+//utskrift av resultatet..
+echo "Totalt antall korn: " . number_format($kornTotalt) . "<br>";
+echo "Total vekt av korn i tonn: ". number_format($kornTonn, 2) . "<br>";
+
+
+?>
